f1fe99fcab141cdeaa02961b3faea3b4f2e2d019
cactusjackk7 <cactusjackk7@gmail.com>
2021-01-28 00:07:40 +0100

    for educational purposes only

---
+++ pod/flooder.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+#this is for eductional only!
+
+from scapy.all import *
+import sys
+
+if len(sys.argv) == 1:
+    print('Usage: python3 flooder_pcap.py <numbers of send> <filename> (Run it as root)')
+
+    n = int(sys.argv[1])
+    filename = sys.argv[2]
+
+    print('Reading pcap file.')
+    pkgs = rdpcap(filename)
+
+    for i in range (n):
+        print('Sending %s packets.' % (len(pkgs)))
+            sendpfast(pkgs)
+                print('Done, part %s of %s' % ((i+1) , n))

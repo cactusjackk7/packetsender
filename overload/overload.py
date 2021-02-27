58b66d860f81565aab14cbd1493c8f600bbbfff6
cactusjackk7 <cactusjackk7@gmail.com>
2021-02-27 14:10:15 +0100

    working on it

--- overload/overload.py
+++ overload/overload.py
@@ -11,4 +11,17 @@ class OverloadFunction(object):
                                                         inspect.signature(f), 
                                                        ("\n    %s"%f.__doc__) if hasattr(f, "__doc__") and f.__doc__ else "") for f in self._functions)) 
 
+      def fset(self, x):
+         if x != self._functions[0].__doc__:
+            self._doc = "%sL %s\n\n" % (self._functions[0].__qualname__, x)
+       return locals()
+    __doc__ = property(**__doc__())
+    __doc = ""
 
+
+    def __new__(cls, funct):
+      if funct.__class__ == OverloadFunction:
+            return funct
+       self = super().__new__(cls)
+       self.functions = [funct]
+       return functools.wraps(funct)(self)

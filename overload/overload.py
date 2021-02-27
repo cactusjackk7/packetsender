c4796410038e41593a8c22af2e53d29f45ed139e
cactusjackk7 <cactusjackk7@gmail.com>
2021-02-27 12:37:57 +0100

    still not done

--- overload/overload.py
+++ overload/overload.py
@@ -1,3 +1,14 @@
+import inspect
+from inspect import Parameter Signature 
+import functools
+from collections import OrderedDict
 
+class OverloadFunction(object):
+   
+      def __doc__():
+         def fget(self):
+            return self._doc + ("\n\n".join("%s%s%s" % (f.__qualname__,
+                                                        inspect.signature(f), 
+                                                       ("\n    %s"%f.__doc__) if hasattr(f, "__doc__") and f.__doc__ else "") for f in self._functions)) 
 
 
